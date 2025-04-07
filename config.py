edc5af46867d37e524c7fd15be5995263746fba3
Pritam Das <trydaspritam@gmail.com>
2025-04-07 23:16:26 +0530

    initial commit

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret_key")  # Ensure it never becomes None
     AUTH_MONGO_URI = os.getenv("AUTH_MONGO_URI", "mongodb://localhost:27017/auth")
