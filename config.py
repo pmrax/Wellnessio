e07e681edb4a64383b48d554b2401f3776938e3f
pmrax <pmondal8884@gmail.com>
2025-04-12 10:49:59 +0530

    update_1.0.0.1 commit

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret_key")  # Ensure it never becomes None
+    SECRET_KEY = os.getenv("SECRET_KEY", "1#23@drc324f2#$%@#$FFC32@$3423")  # Ensure it never becomes None
+    
+    # MongoDB database connection in local system.
     AUTH_MONGO_URI = os.getenv("AUTH_MONGO_URI", "mongodb://localhost:27017/auth")
+
+
+
